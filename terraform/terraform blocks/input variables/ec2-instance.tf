cef9fe0d5475cd380a020ac6130eedd55563d47d
amalachennu <amalaradhakrishna2@gmail.com>
2024-09-09 18:04:51 -0400

    input variables folder

---
+++ terraform/terraform blocks/input variables/ec2-instance.tf
@@ -0,0 +1,17 @@
+# Create EC2 Instance
+resource "aws_instance" "my-ec2-vm" {
+  ami           = var.ec2_ami_id
+  instance_type = var.ec2_instance_type
+  count         = var.ec2_instance_count
+  user_data     = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<h1> Welcome To Devops | AWS Infra Created Using Terraform in us-east- Region </h1>
+    EOF
+  tags = {
+    "Name" = "myc2vm"
+  }
+}
