c3e58fe0439142a6858eefd4b4ae2503bb417f7a
amalachennu <amalaradhakrishna2@gmail.com>
2024-09-13 16:49:52 -0400

    local values

---
+++ terraform/terraform blocks/local values block/c2-variables.tf
@@ -0,0 +1,16 @@
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS Resource to be created"
+  type        = string
+  default     = "us-east-1"
+}
+# App Name S3 Bucket used for
+variable "aws_region" {
+  description = "Application Name"
+  type        = string
+}  
+# Environment Name
+variable "aws_region" {
+  description = "Environment Name"
+  type        = string
+}  
