550ea05b8227ace32ab9d592b63be4f822386353
amalachennu <amalaradhakrishna2@gmail.com>
2024-09-18 08:24:21 -0400

    data source block files

---
+++ terraform/terraform blocks/Data sources blocks/c4-ec2-instance.tf
@@ -0,0 +1,11 @@
+# Create EC2 Instance - Amazon Linux
+resource "aws_instance" "my-ec2-vm" {
+    ami               = data.aws_ami.amzlinux.id 
+    instance_type     = var.ec2_instance_type
+    key_name          = "pem file name without .pem"
+    user_data         = file("apache-install.sh")
+    vpc_security_group_ids = [aws_security_group.S.id, aws_security_group.W.id]
+    tags  = {
+        "Name" = "amz-linux-vm"
+    }
+}
