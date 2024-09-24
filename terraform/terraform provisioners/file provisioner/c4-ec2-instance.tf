233a2e54efc02fad3f979c21045661c7d8953f9d
amalachennu <amalaradhakrishna2@gmail.com>
2024-09-24 17:39:43 -0400

    just some modification

--- terraform/terraform provisioners/file provisioner/c4-ec2-instance.tf
+++ terraform/terraform provisioners/file provisioner/c4-ec2-instance.tf
@@ -20,8 +20,8 @@ resource "aws_instance" "my-ec2-vm" {
    }
   # Copies the file-copy.html file to /tmp/file-copy.html
    Provisioner "file" {
-     source      = "apps/file-cop.html"
-     destination = "/tmp/file-copy.html"
+     source      = "apps/file-copy.html"
+     destination = "/tmp/file-copy.html"  # "/var/www/html/file-copy.html" if you want to use apache to see it in browser
   }
   # Copies the string content into /tmp/file.log  
    Provisioner "file" {
@@ -31,11 +31,11 @@ resource "aws_instance" "my-ec2-vm" {
    # copies the app1 folder to /tmp - FOLDER COPY
    Provisioner "file" {
      source      = "apps/app1"
-     destination = "/tmp"
+     destination = "/tmp" # "/var/www/html/apps/app1" if you want to use apache to see it in browser
    } 
    # Copies all files and folders in apps/app2 to /tmp - CONTENTS OF FOLDER WILL BE COPIED
    Provisioner "file" {
      source      = "apps/app2/" # When "/" at the end is added  - CONTENTS of FOLDER WILL BE COPIED
-     destination = "/tmp"
+     destination = "/tmp"  # "/var/www/html/apps/app2" if you want to use apache to see it in browser
    }
 }
