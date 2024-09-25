f2a025491c90941ff6ca1c9cbb264da1075352a8
amalachennu <amalaradhakrishna2@gmail.com>
2024-09-25 17:46:44 -0400

    tf module

---
+++ terraform/terraform modules/build a module/c3-s3bucket.tf
@@ -0,0 +1,6 @@
+# Call our Custom Terraform Module Which We build earlier
+module "website_s3_bucket" {
+    source       = "./modules/aws-s3-static-website-bucket" # Mandatory
+    bucket_name  = var.my_s3_bucket
+    tags         = var.my_s3_tags
+}
