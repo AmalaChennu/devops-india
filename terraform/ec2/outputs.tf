3976a5c1e4f83dc1e05efa7d16c30b52db867519
amalachennu <amalaradhakrishna2@gmail.com>
2024-10-01 17:52:21 -0400

    tf module file

---
+++ terraform/ec2/outputs.tf
@@ -0,0 +1,16 @@
+output "ec2_public_ip" {
+    value = aws_instance.app_server[*].public_ip
+}
+
+output "ec2_tags" {
+    value = aws_instance.app_server[*].tags_all
+}
+
+/* 
+output "ec2_piblic_ip" {
+     value = aws_instance.app_server[*].public_ip
+}
+output "ec2_tags" {
+     value = aws_instance.app_server[*].tags_all
+}
+*/
---
+++ terraform/terraform s3 bucket module/provider.tf
@@ -0,0 +1,4 @@
+provider "aws"{
+ profile = "profilename"
+ region = "us-east-1"
+}
